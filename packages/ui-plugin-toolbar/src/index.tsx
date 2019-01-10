e811ca7353f6cb7c4515c0149ab40c7df6c88b1c
metzgaria <mele-metzger@web.de>
2019-01-10 11:58:57 +0100

    sticky toolbar implemented

--- packages/ui-plugin-toolbar/src/index.tsx
+++ packages/ui-plugin-toolbar/src/index.tsx
@@ -1,8 +1,24 @@
 import * as React from 'react'
-
+import { Sticky, StickyContainer } from 'react-sticky'
 export * from './button'
 export * from './dropdown'
 
 export const renderIntoToolbar = (children: React.ReactNode) => {
-  return <div>{children}</div>
+  return (
+    <Sticky>
+      {({
+        style,
+        isSticky
+      }: {
+        style: React.CSSProperties
+        isSticky: boolean
+      }) => {
+        return isSticky ? (
+          <div style={{ ...style }}>{children}</div>
+        ) : (
+          <div>{children}</div>
+        )
+      }}
+    </Sticky>
+  )
 }
